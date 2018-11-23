4a3f2ebf1aea931a437e9733ce7b0ed37f6a9110
Anis Benyoub <anis@unity3d.com>
2018-11-23 14:55:36 +0100

    Moving Code around so that it can be used in the raytracing code

--- com.unity.render-pipelines.high-definition/Runtime/Material/MaterialEvaluation.hlsl
+++ com.unity.render-pipelines.high-definition/Runtime/Material/MaterialEvaluation.hlsl
@@ -1,40 +1,5 @@
 // This files include various function uses to evaluate material
-
-//-----------------------------------------------------------------------------
-// Lighting structure for light accumulation
-//-----------------------------------------------------------------------------
-
-// These structure allow to accumulate lighting accross the Lit material
-// AggregateLighting is init to zero and transfer to EvaluateBSDF, but the LightLoop can't access its content.
-struct DirectLighting
-{
-    float3 diffuse;
-    float3 specular;
-};
-
-struct IndirectLighting
-{
-    float3 specularReflected;
-    float3 specularTransmitted;
-};
-
-struct AggregateLighting
-{
-    DirectLighting   direct;
-    IndirectLighting indirect;
-};
-
-void AccumulateDirectLighting(DirectLighting src, inout AggregateLighting dst)
-{
-    dst.direct.diffuse += src.diffuse;
-    dst.direct.specular += src.specular;
-}
-
-void AccumulateIndirectLighting(IndirectLighting src, inout AggregateLighting dst)
-{
-    dst.indirect.specularReflected += src.specularReflected;
-    dst.indirect.specularTransmitted += src.specularTransmitted;
-}
+#include "Packages/com.unity.render-pipelines.high-definition/Runtime/Material/LightAccumulation.hlsl"
 
 //-----------------------------------------------------------------------------
 // Ambient occlusion helper
